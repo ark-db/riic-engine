9b4f96c179d8fb92681c852b570b99365e0802f9
astral4 <88992929+astral4@users.noreply.github.com>
2022-11-02 10:08:21 -0400

    add fallback error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div>
+	<h1>{$page.status} <span>|</span> {$page.error?.message}</h1>
+	<p>An unexpected error occurred.</p>
+	<a href="/">Return to main menu</a>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	h1,
+	p,
+	a {
+		color: var(--light);
+		text-align: center;
+	}
+	h1 {
+		border-radius: 0.5em;
+		padding: 0.5em;
+		background-color: var(--dark-strong);
+		font-size: 3em;
+	}
+	span {
+        margin: 0 0.2em;
+		font-weight: 100;
+	}
+	a {
+		margin: 2em auto 0;
+		border-radius: 0.5em;
+		padding: 1em;
+		background-color: var(--blue);
+		text-decoration: none;
+	}
+	a:hover {
+		background-color: var(--blue-strong);
+	}
+</style>
