b0454d209120315d4d3a3ae8660c4b76d18af974
astral4 <88992929+astral4@users.noreply.github.com>
2022-11-05 18:47:05 -0400

    prevent page overflow for elements too far right

--- src/tooltip.ts
+++ src/tooltip.ts
@@ -9,25 +9,21 @@ const tooltipStyle = `
 export function tooltip(element: HTMLElement) {
 	let div: HTMLDivElement;
 	let title: string | null;
-	let position: DOMRect;
-	function focus() {
-		position = element.getBoundingClientRect();
-		// remove the `title` attribute to prevent showing the default browser tooltip
-		title = element.getAttribute('title');
-		element.removeAttribute('title');
 
-		div = document.createElement('div');
-		div.textContent = title;
-		div.setAttribute(
-			'style',
-			`
-            ${tooltipStyle}
-            top: ${position.top + 10}px;
-            left: ${position.left + 5}px;
-        `
-		);
-		document.body.appendChild(div);
+	function calcPosition(event: MouseEvent): [number, number] {
+		let left = event.pageX + 5;
+		let top = event.pageY + 5;
+		// check for page overflow
+		if (left + div.clientWidth > document.documentElement.clientWidth) {
+			left = event.pageX - div.clientWidth + 5;
+		}
+		if (top + div.clientHeight > document.documentElement.clientHeight) {
+			top = event.pageY - div.clientHeight + 5;
+		}
+
+		return [left, top];
 	}
+
 	function mouseOver(event: MouseEvent) {
 		// remove the `title` attribute to prevent showing the default browser tooltip
 		title = element.getAttribute('title');
@@ -35,19 +31,23 @@ export function tooltip(element: HTMLElement) {
 
 		div = document.createElement('div');
 		div.textContent = title;
+
+		let [left, top] = calcPosition(event);
 		div.setAttribute(
 			'style',
 			`
             ${tooltipStyle}
-            top: ${event.pageX + 5}px;
-            left: ${event.pageY + 5}px;
+			left: ${left}px;
+			top: ${top}px;
         `
 		);
+
 		document.body.appendChild(div);
 	}
 	function mouseMove(event: MouseEvent) {
-		div.style.left = `${event.pageX + 5}px`;
-		div.style.top = `${event.pageY + 5}px`;
+		let [left, top] = calcPosition(event);
+		div.style.left = `${left}px`;
+		div.style.top = `${top}px`;
 	}
 	function hide() {
 		document.body.removeChild(div);
@@ -55,19 +55,16 @@ export function tooltip(element: HTMLElement) {
 		element.setAttribute('title', title ?? '');
 	}
 
-	element.addEventListener('focus', focus);
-	element.addEventListener('blur', hide);
 	element.addEventListener('mouseover', mouseOver);
 	element.addEventListener('mouseleave', hide);
 	element.addEventListener('mousemove', mouseMove);
 
 	return {
 		destroy() {
-			element.removeEventListener('focus', focus);
-			element.removeEventListener('blur', hide);
 			element.removeEventListener('mouseover', mouseOver);
 			element.removeEventListener('mouseleave', hide);
 			element.removeEventListener('mousemove', mouseMove);
+			hide();
 		}
 	};
 }
