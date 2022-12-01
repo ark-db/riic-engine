453cc9bd9cc27b2d06d3c70e51c46e422cd755d4
astral4 <88992929+astral4@users.noreply.github.com>
2022-12-01 00:26:14 -0500

    add some type hints

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,10 @@
 from enum import Enum
-import requests
-from operator import itemgetter
+from requests import Session
 from pathlib import Path
 from PIL import Image
 from io import BytesIO
 import warnings
+from operator import itemgetter
 import json
 
 
@@ -33,13 +33,14 @@ NAME_CHANGES = {
 }
 
 
-def is_operator(char_info):
+# The type hint for char_info is lenient because declaring the entire schema is too painful
+def is_operator(char_info: dict) -> bool:
     return char_info["profession"] != "TOKEN" \
         and char_info["profession"] != "TRAP" \
         and not char_info["isNotObtainable"]
 
 
-def save_image(session, category, name):
+def save_image(session: Session, category: Asset, name: str) -> None:
     target_path = Path(f"./static/{category.value['dir']}/{name}.webp")
 
     if target_path.is_file():
@@ -59,34 +60,35 @@ def save_image(session, category, name):
 
 char_data = []
 
-with requests.Session() as s:
-    chars = (
+with Session() as s:
+    CHARS = (
         s.get(
             "https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json")
         .json()
     )
 
-    cn_char_skills, cn_skill_data = itemgetter("chars", "buffs")(
+    CN_CHAR_SKILLS, CN_SKILL_DATA = itemgetter("chars", "buffs")(
         s.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/building_data.json")
         .json()
     )
 
-    en_skill_data = (
+    EN_SKILL_DATA = (
         s.get("https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/en_US/gamedata/excel/building_data.json")
         .json()
         ["buffs"]
     )
 
-    for char_id, data in chars.items():
+    for char_id, data in CHARS.items():
         if is_operator(data):
             skills = []
-            for skill in cn_char_skills[char_id]["buffChar"]:
+            for skill in CN_CHAR_SKILLS[char_id]["buffChar"]:
                 for tier in skill["buffData"]:
                     level_req = tier["cond"]
-                    skill_info = en_skill_data.get(
+                    skill_info = EN_SKILL_DATA.get(
                         tier["buffId"],
-                        cn_skill_data[tier["buffId"]]
+                        CN_SKILL_DATA[tier["buffId"]]
                     )
+
                     icon_id = skill_info["skillIcon"]
                     skills.append({
                         "elite": level_req["phase"],
