01a27c13cb29973d9f0d015f371605846a7bb3ac
astral4 <88992929+astral4@users.noreply.github.com>
2023-02-12 22:44:24 -0500

    bugfix: style prop

--- src/routes/editor/editor/RowWrapper.svelte
+++ src/routes/editor/editor/RowWrapper.svelte
@@ -11,7 +11,8 @@
 			g = parseInt(hex.slice(3, 5), 16),
 			b = parseInt(hex.slice(5, 7), 16);
 
-		return `rgb(${r} ${g} ${b} ${alpha})`;
+		// values need to be separated by commas when passed as a style prop
+		return `rgb(${r}, ${g}, ${b}, ${alpha})`;
 	}
 </script>
 
