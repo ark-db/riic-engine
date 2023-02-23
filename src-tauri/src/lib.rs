2fc84fd9fdb0c1a505cec211d13e8c751ec53b2d
astral4 <88992929+astral4@users.noreply.github.com>
2023-02-22 22:52:42 -0500

    update lints; make errors opaque

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,35 @@
-#![warn(clippy::all, clippy::pedantic)]
+#![warn(clippy::pedantic, future_incompatible, nonstandard_style, unused)]
+#![deny(
+    let_underscore_drop,
+    macro_use_extern_crate,
+    non_ascii_idents,
+    noop_method_call,
+    pointer_structural_match,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    unused_crate_dependencies,
+    unused_import_braces,
+    unused_qualifications
+)]
 
 mod base;
 pub mod savefile;
 pub mod window;
 
-#[derive(Debug, thiserror::Error)]
+use serde::Serialize;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
 pub enum CmdError {
-    #[error(transparent)]
+    #[error("A system I/O error occurred")]
     Io(#[from] std::io::Error),
 
-    #[error(transparent)]
+    #[error("A system time error occurred")]
     Time(#[from] std::time::SystemTimeError),
 
-    #[error(transparent)]
+    #[error("An error occurred while writing the save file")]
     FileWrite(#[from] serde_json::Error),
 
     #[error("No name specified")]
@@ -22,7 +39,7 @@ pub enum CmdError {
     DuplicateName,
 }
 
-impl serde::Serialize for CmdError {
+impl Serialize for CmdError {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::ser::Serializer,
