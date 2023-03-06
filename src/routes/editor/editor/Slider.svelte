6a0c0b099496ec65a6176fc82af8f941228c947a
astral4 <88992929+astral4@users.noreply.github.com>
2023-03-05 21:38:21 -0500

    custom slider component

---
+++ src/routes/editor/editor/Slider.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	export let value: number;
+	export let min: number;
+	export let max: number;
+	export let step: number;
+</script>
+
+<input type="range" bind:value {min} {max} {step} />
+
+<style>
+	input {
+		-webkit-appearance: none;
+		appearance: none;
+		background: transparent;
+		cursor: pointer;
+		width: 15rem;
+		--track-height: 0.4em;
+		--track-border-radius: calc(var(--track-height) / 2);
+		--track-color: var(--dark);
+		--thumb-height: 2em;
+		--thumb-width: 1em;
+		--thumb-border-radius: 0.25em;
+		--thumb-color: var(--light-strong);
+		--focus-outline: 2.5px solid var(--blue-strong);
+		--focus-offset: 2px;
+	}
+	/* Track styles */
+	input::-webkit-slider-runnable-track {
+		height: var(--track-height);
+		border-radius: var(--track-border-radius);
+		background: var(--track-color);
+	}
+	input::-moz-range-track {
+		height: var(--track-height);
+		border-radius: var(--track-border-radius);
+		background: var(--track-color);
+	}
+	/* Thumb styles */
+	input::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		height: var(--thumb-height);
+		width: var(--thumb-width);
+		margin-top: calc(var(--track-height) / 2 - var(--thumb-height) / 2);
+		border-radius: var(--thumb-border-radius);
+		background-color: var(--thumb-color);
+	}
+	input::-moz-range-thumb {
+		height: var(--thumb-height);
+		width: var(--thumb-width);
+		border: none;
+		border-radius: var(--thumb-border-radius);
+		background-color: var(--thumb-color);
+	}
+	/* Focus styles */
+	input:focus {
+		outline: none;
+	}
+	input:focus::-webkit-slider-thumb {
+		outline: var(--focus-outline);
+		outline-offset: var(--focus-offset);
+	}
+	input:focus::-moz-range-thumb {
+		outline: var(--focus-outline);
+		outline-offset: var(--focus-offset);
+	}
+</style>
