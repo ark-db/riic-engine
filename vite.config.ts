f5bb8e8982a1c2532c6ebf7494f006dce6dfa099
astral4 <88992929+astral4@users.noreply.github.com>
2023-03-06 18:54:20 -0500

    update build target config

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,13 @@ const config: UserConfig = {
 	json: {
 		stringify: true
 	},
-	// By default, Vite inlines assets smaller than 4 KiB as base64 (see https://vitejs.dev/config/build-options.html#build-assetsinlinelimit)
-	// These assets are blocked by the CSP in production builds, so inlining is explicitly disabled
 	build: {
+		// The Windows webview (WebView2) is supposed to update itself automatically, so it's safe to target the latest 3 versions (i.e. Blink 108).
+		// The macOS webview (WebKit) only updates with new OS versions, so the build target is more conservative; the earliest Safari version for macOS Big Sur is 14 (i.e. WebKit 610.2.11).
+		// The Linux webview (WebKitGTK) is based on WebKit, so the target is the same as macOS.
+		target: process.env.TAURI_PLATFORM == 'windows' ? 'edge108' : ['es2021', 'safari14'],
+		// By default, Vite inlines assets smaller than 4 KiB as base64 (see https://vitejs.dev/config/build-options.html#build-assetsinlinelimit)
+		// These assets are blocked by the CSP in production builds, so inlining is explicitly disabled
 		assetsInlineLimit: 0
 	},
 	server: {
