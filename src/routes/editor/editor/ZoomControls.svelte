112ac8f8af6e7035b484e886075e71c11df4e928
astral4 <88992929+astral4@users.noreply.github.com>
2023-03-07 22:39:48 -0500

    clean up slider control component css (1/?)

--- src/routes/editor/editor/ZoomControls.svelte
+++ src/routes/editor/editor/ZoomControls.svelte
@@ -11,7 +11,6 @@
 	export let xScale: number;
 	export let yScale: number;
 
-	const iconSize = 22;
 	const minZoom = 1;
 	const maxZoom = 4;
 	const zoomStep = 0.25;
@@ -59,7 +58,7 @@
 	}
 </script>
 
-<div class="controls" style="--zoom-icon-size: {iconSize}px;">
+<div class="controls">
 	<div class="center">
 		<Button desc={centerDesc} onClick={centerFn}>
 			<img src={centerSrc} alt={centerDesc} width="36" height="36" />
@@ -67,44 +66,37 @@
 	</div>
 	<div class="slider x">
 		<Button desc="Zoom out" onClick={decrementX}>
-			<img src={minus} alt="Zoom out" width={iconSize} height={iconSize} />
+			<img src={minus} alt="Zoom out" width="22" height="22" />
 		</Button>
 		<Slider min={minZoom} max={maxZoom} step={0.002} bind:value={$x} />
 		<Button desc="Zoom in" onClick={incrementX}>
-			<img src={plus} alt="Zoom in" width={iconSize} height={iconSize} />
+			<img src={plus} alt="Zoom in" width="22" height="22" />
 		</Button>
 	</div>
 	<div class="slider y">
 		<Button desc="Zoom out" onClick={decrementY}>
-			<img src={minus} alt="Zoom out" width={iconSize} height={iconSize} />
+			<img src={minus} alt="Zoom out" width="22" height="22" />
 		</Button>
 		<Slider min={minZoom} max={maxZoom} step={0.002} bind:value={$y} />
 		<Button desc="Zoom in" onClick={incrementY}>
-			<img src={plus} alt="Zoom in" width={iconSize} height={iconSize} />
+			<img src={plus} alt="Zoom in" width="22" height="22" />
 		</Button>
 	</div>
 </div>
 
 <style>
 	.controls {
-		--right-shift: 48px;
-		--slider-length: 15em;
+		--center-padding: 0.75em;
 		--slider-padding: 1.5em;
 		--slider-spacing: 0.75em;
-		--center-size: 32px;
-		--center-margin: 16px;
-		--shift-distance: calc(
-			var(--slider-length) / 2 + var(--slider-spacing) + var(--zoom-icon-size) * 2 +
-				var(--center-size) + var(--center-margin)
-		);
 		z-index: 2;
 		position: fixed;
-		bottom: 0;
-		right: calc(0px - var(--right-shift) - var(--slider-padding));
+		right: 80px;
+		bottom: 24px;
 	}
 	.controls > div {
 		--blur-effect: blur(2px);
-		position: relative;
+		position: absolute;
 		box-shadow: 0 4px 12px rgba(0 0 0 / 0.2);
 		border: 1px solid rgba(255 255 255 / 0.25);
 		border-radius: 16px;
@@ -113,14 +105,8 @@
 		-webkit-backdrop-filter: var(--blur-effect);
 	}
 	.controls > .center {
-		--icon-width: 36px;
-		--center-padding: 0.75em;
-		--center-shift: calc(0px - var(--icon-width) - var(--slider-padding) * 2);
-		width: var(--icon-width);
-		right: calc(var(--center-shift) - 0.75em + var(--center-padding) - var(--slider-spacing));
-		bottom: calc(
-			var(--center-shift) - 1.375em - var(--center-padding) / 2 - var(--slider-spacing) / 2
-		);
+		right: 0;
+		bottom: 0;
 		padding: var(--center-padding);
 	}
 	.controls > .slider {
@@ -130,13 +116,14 @@
 		column-gap: var(--slider-spacing);
 	}
 	.controls > .x {
-		right: var(--shift-distance);
-		bottom: calc(0px - 2 * var(--slider-padding));
+		right: 0;
+		bottom: 0;
 	}
 	.controls > .y {
 		rotate: 90deg;
-		right: calc(var(--center-size) / 2);
-		bottom: var(--shift-distance);
+		transform-origin: right;
+		right: 0;
+		bottom: 0;
 	}
 	.controls > .y img {
 		rotate: -90deg;
