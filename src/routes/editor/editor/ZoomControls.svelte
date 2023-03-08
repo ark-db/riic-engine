f5fad04ac8a58fd74eef534852d30453b34ad0bb
astral4 <88992929+astral4@users.noreply.github.com>
2023-03-07 22:15:22 -0500

    add toggle button for max/min zoom

--- src/routes/editor/editor/ZoomControls.svelte
+++ src/routes/editor/editor/ZoomControls.svelte
@@ -2,6 +2,8 @@
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
 	import Button from '$lib/components/Button.svelte';
+	import maximize from '$lib/images/maximize.svg';
+	import minimize from '$lib/images/minimize.svg';
 	import plus from '$lib/images/plus.svg';
 	import minus from '$lib/images/minus.svg';
 	import Slider from './Slider.svelte';
@@ -26,6 +28,23 @@
 	$: xScale = $x;
 	$: yScale = $y;
 
+	function setMaxZoom() {
+		$x = maxZoom;
+		$y = maxZoom;
+	}
+	function setMinZoom() {
+		$x = minZoom;
+		$y = minZoom;
+	}
+
+	type ZoomMode = 'max' | 'min';
+	let centerZoomMode: ZoomMode = 'max';
+	$: if ($x === minZoom && $y === minZoom) centerZoomMode = 'max';
+	$: if ($x === maxZoom && $y === maxZoom) centerZoomMode = 'min';
+	$: centerDesc = centerZoomMode === 'max' ? 'Zoom to maximum' : 'Zoom to minimum';
+	$: centerSrc = centerZoomMode === 'max' ? maximize : minimize;
+	$: centerFn = centerZoomMode === 'max' ? setMaxZoom : setMinZoom;
+
 	function incrementX() {
 		if ($x < maxZoom) $x = Math.min($x + zoomStep, maxZoom);
 	}
@@ -41,7 +60,12 @@
 </script>
 
 <div class="controls" style="--zoom-icon-size: {iconSize}px;">
-	<div class="x">
+	<div class="center">
+		<Button desc={centerDesc} onClick={centerFn}>
+			<img src={centerSrc} alt={centerDesc} width="36" height="36" />
+		</Button>
+	</div>
+	<div class="slider x">
 		<Button desc="Zoom out" onClick={decrementX}>
 			<img src={minus} alt="Zoom out" width={iconSize} height={iconSize} />
 		</Button>
@@ -50,7 +74,7 @@
 			<img src={plus} alt="Zoom in" width={iconSize} height={iconSize} />
 		</Button>
 	</div>
-	<div class="y">
+	<div class="slider y">
 		<Button desc="Zoom out" onClick={decrementY}>
 			<img src={minus} alt="Zoom out" width={iconSize} height={iconSize} />
 		</Button>
@@ -63,7 +87,7 @@
 
 <style>
 	.controls {
-		--right-shift: 40px;
+		--right-shift: 48px;
 		--slider-length: 15em;
 		--slider-padding: 1.5em;
 		--slider-spacing: 0.75em;
@@ -79,11 +103,25 @@
 		right: calc(0px - var(--right-shift) - var(--slider-padding));
 	}
 	.controls > div {
-		--blur-effect: blur(2px);
 		position: relative;
 		box-shadow: 0 4px 12px rgba(0 0 0 / 0.2);
 		border: 1px solid rgba(255 255 255 / 0.25);
 		border-radius: 16px;
+	}
+	.controls > .center {
+		--icon-width: 36px;
+		--center-padding: 0.75em;
+		--center-shift: calc(0px - var(--icon-width) - var(--slider-padding) * 2);
+		width: var(--icon-width);
+		right: calc(var(--center-shift) - 0.75em + var(--center-padding) - var(--slider-spacing));
+		bottom: calc(
+			var(--center-shift) - 1.375em - var(--center-padding) / 2 - var(--slider-spacing) / 2
+		);
+		padding: var(--center-padding);
+		background-color: var(--dark);
+	}
+	.controls > .slider {
+		--blur-effect: blur(2px);
 		padding: var(--slider-padding);
 		background: rgba(255 255 255 / 0.04);
 		backdrop-filter: var(--blur-effect);
