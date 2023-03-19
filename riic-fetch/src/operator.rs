e584da8afed3387d46029ee02e76f8fe6d12cf00
astral4 <88992929+astral4@users.noreply.github.com>
2023-03-19 15:39:32 -0400

    deserialize ops

---
+++ riic-fetch/src/operator.rs
@@ -0,0 +1,52 @@
+use ahash::HashMap;
+use serde::{de, Deserialize};
+
+const OPERATOR_URL: &str = "https://raw.githubusercontent.com/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json";
+
+#[derive(Deserialize)]
+struct OperatorData<'a> {
+    #[serde(flatten, borrow)]
+    inner: HashMap<&'a str, OperatorInner<'a>>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct OperatorInner<'a> {
+    #[serde(rename = "appellation")]
+    name: &'a str,
+    #[serde(deserialize_with = "deserialize_rarity")]
+    rarity: u8,
+    #[serde(rename = "isNotObtainable")]
+    unobtainable: bool,
+    profession: Profession,
+}
+
+fn deserialize_rarity<'de, D>(deserializer: D) -> Result<u8, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let num: u8 = Deserialize::deserialize(deserializer)?;
+
+    match num {
+        n @ 0..=5 => Ok(n + 1),
+        other => Err(de::Error::invalid_value(
+            de::Unexpected::Unsigned(u64::from(other)),
+            &"Expected a u8 from 0 to 5",
+        )),
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+enum Profession {
+    Vanguard,
+    Guard,
+    Defender,
+    Sniper,
+    Caster,
+    Medic,
+    Supporter,
+    Specialist,
+    Token,
+    Trap,
+}
