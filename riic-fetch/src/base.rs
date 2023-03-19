0c9f3216b0a1ec442f1fdb1f2e80aaf7f0b76465
astral4 <88992929+astral4@users.noreply.github.com>
2023-03-19 17:29:52 -0400

    deserialize char skills

--- riic-fetch/src/base.rs
+++ riic-fetch/src/base.rs
@@ -1,24 +1,88 @@
 use crate::Fetch;
 use ahash::HashMap;
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Serialize};
+
+type CharSkills<'a> = HashMap<&'a str, Vec<BaseSkill<'a>>>;
 
 #[derive(Deserialize)]
 struct BaseData<'a> {
-    chars: u8, // TODO
+    #[serde(deserialize_with = "deserialize_skills")]
+    chars: CharSkills<'a>,
     rooms: u8, // TODO
     #[serde(borrow)]
     buffs: SkillTable<'a>,
 }
 
+#[derive(Deserialize)]
+struct UnprocessedCharEntry<'a> {
+    #[serde(borrow, rename = "buffChar")]
+    inner: Vec<UnprocessedSkillSet<'a>>,
+}
+
+#[derive(Deserialize)]
+struct UnprocessedSkillSet<'a> {
+    #[serde(borrow, rename = "buffChar")]
+    inner: Vec<UnprocessedSkill<'a>>,
+}
+
+#[derive(Deserialize)]
+struct UnprocessedSkill<'a> {
+    #[serde(rename = "buffId")]
+    id: &'a str,
+    cond: UnprocessedSkillPhase,
+}
+
+#[derive(Deserialize)]
+struct UnprocessedSkillPhase {
+    #[serde(rename = "phase")]
+    elite: u8,
+    level: u8,
+}
+
+struct BaseSkill<'a> {
+    id: &'a str,
+    elite: u8,
+    level: u8,
+}
+
+fn deserialize_skills<'de, D>(deserializer: D) -> Result<CharSkills<'de>, D::Error>
+where
+    D: de::Deserializer<'de>,
+{
+    let data: HashMap<&'de str, UnprocessedCharEntry<'de>> =
+        Deserialize::deserialize(deserializer)?;
+
+    Ok(data
+        .into_iter()
+        .map(|(char_id, data)| (char_id, data.into()))
+        .collect())
+}
+
+impl<'a> From<UnprocessedCharEntry<'a>> for Vec<BaseSkill<'a>> {
+    fn from(value: UnprocessedCharEntry<'a>) -> Self {
+        let mut skills = Vec::with_capacity(2);
+        for set in value.inner {
+            for skill in set.inner {
+                skills.push(BaseSkill {
+                    id: skill.id,
+                    elite: skill.cond.elite,
+                    level: skill.cond.level,
+                });
+            }
+        }
+        skills
+    }
+}
+
 #[derive(Deserialize, Serialize)]
 struct SkillTable<'a> {
     #[serde(flatten, borrow)]
-    inner: HashMap<&'a str, SkillData<'a>>,
+    inner: HashMap<&'a str, SkillInfo<'a>>,
 }
 
 #[derive(Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-struct SkillData<'a> {
+struct SkillInfo<'a> {
     #[serde(rename(deserialize = "buffName"))]
     name: &'a str,
     #[serde(rename(deserialize = "description"))]
