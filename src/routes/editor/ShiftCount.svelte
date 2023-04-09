6af646dbcb1b4d1c3eca657917e0f31bc485e8da
astral4 <88992929+astral4@users.noreply.github.com>
2023-04-08 20:27:48 -0400

    decrease maximum shift count

--- src/routes/editor/ShiftCount.svelte
+++ src/routes/editor/ShiftCount.svelte
@@ -7,11 +7,11 @@
 <NumberInput
 	desc="Number of shifts"
 	min={0}
-	max={999}
+	max={99}
 	initial={$activeSave.data.maxShift}
 	placeholder=" "
 	onValidInput={(value) => ($activeSave.data.maxShift = value)}
-	errorMsg="The specified number of shifts should be a number from 0 to 999"
+	errorMsg="The specified number of shifts should be a number from 0 to 99"
 	iconSrc={briefcase}
 	iconSize={36}
 />
