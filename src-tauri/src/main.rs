da8495917578f17ff8d90e4387e2b3d2aa483517
astral4 <88992929+astral4@users.noreply.github.com>
2023-04-10 00:47:07 -0400

    qualify names further

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,12 @@
 )]
 
 use riic_engine::{db, window};
+use tauri::{generate_context, generate_handler, Builder};
 
 fn main() {
-    tauri::Builder::default()
+    Builder::new()
         .manage(db::Database::init().expect("Failed to initialize app database"))
-        .invoke_handler(tauri::generate_handler![
+        .invoke_handler(generate_handler![
             window::show_window,
             window::rename_window,
             db::fetch_saves,
@@ -19,6 +20,6 @@ fn main() {
             db::delete_save,
             db::export_save
         ])
-        .run(tauri::generate_context!())
+        .run(generate_context!())
         .expect("An error occurred while running the application.");
 }
