c4c45d54bc20ff251b51425c706c79001a2e97fb
astral4 <88992929+astral4@users.noreply.github.com>
2023-04-24 10:54:34 -0400

    rename import

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -1,11 +1,11 @@
 #![allow(clippy::module_name_repetitions, clippy::needless_pass_by_value)]
 
-use tauri::{Manager, Result as TauriResult, Window};
+use tauri::{Manager, Result, Window};
 
 /// # Errors
 /// Returns error if the main window cannot be shown.
 #[tauri::command]
-pub fn show_window(window: Window) -> TauriResult<()> {
+pub fn show_window(window: Window) -> Result<()> {
     let w = window.get_window("main").expect("Failed to get app window");
     w.show()
 }
@@ -13,7 +13,7 @@ pub fn show_window(window: Window) -> TauriResult<()> {
 /// # Errors
 /// Returns error if the main window title cannot be renamed.
 #[tauri::command]
-pub fn rename_window(window: Window, name: Option<&str>) -> TauriResult<()> {
+pub fn rename_window(window: Window, name: Option<&str>) -> Result<()> {
     let w = window.get_window("main").expect("Failed to get app window");
     match name {
         Some(n) => w.set_title(&format!("RIIC Engine • {n}")),
