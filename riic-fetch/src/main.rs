356af2dc547f3e3f915d1df5b835f1d81ac3a16a
astral4 <88992929+astral4@users.noreply.github.com>
2023-05-03 21:56:47 -0400

    tweak command; set current directory to riic-fetch

--- riic-fetch/src/main.rs
+++ riic-fetch/src/main.rs
@@ -1,7 +1,10 @@
 use riic_fetch::Config;
+use std::env::set_current_dir;
 
 #[tokio::main]
 async fn main() {
+    set_current_dir("riic-fetch").expect("Failed to set current directory to /riic-fetch");
+
     Config::from_toml("config.toml")
         .expect("Failed to read configuration file")
         .fetch()
