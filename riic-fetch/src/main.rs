f2ce8b7c7fe875d10db51e0b27c383db665e1df6
astral4 <88992929+astral4@users.noreply.github.com>
2023-05-06 12:22:59 -0400

    let program run if current dir is already the package dir

--- riic-fetch/src/main.rs
+++ riic-fetch/src/main.rs
@@ -1,9 +1,14 @@
 use riic_fetch::Config;
-use std::env::set_current_dir;
+use std::env::{current_dir, set_current_dir};
 
 #[tokio::main]
 async fn main() {
-    set_current_dir("riic-fetch").expect("Failed to set current directory to /riic-fetch");
+    if !current_dir()
+        .expect("Failed to get current directory")
+        .ends_with("riic-fetch")
+    {
+        set_current_dir("riic-fetch").expect("Failed to set current directory to /riic-fetch");
+    }
 
     Config::from_toml("config.toml")
         .expect("Failed to read configuration file")
