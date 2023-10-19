3990a41a52252614ff2001bbd511841c1e2bb8c4
astral4 <88992929+astral4@users.noreply.github.com>
2023-10-18 23:46:03 -0400

    typo fixes (continued)

--- src/lib/components/GradientContainer.svelte
+++ src/lib/components/GradientContainer.svelte
@@ -16,8 +16,8 @@
 		background-color: var(--bg-color);
 		background-clip: content-box;
 	}
-	div:after,
-	div:before {
+	div::before,
+	div::after {
 		position: absolute;
 		top: 0;
 		bottom: 0;
@@ -26,16 +26,16 @@
 		content: '';
 		border-radius: inherit;
 	}
-	div:after {
+	div::before {
+		background: linear-gradient(to right, var(--blue-strong), var(--blue-mild));
+		z-index: -2;
+	}
+	div::after {
 		background: inherit;
 		z-index: -1;
 		transition: opacity 0.25s;
 	}
-	div:before {
-		background: linear-gradient(to right, var(--blue-strong), var(--blue-mild));
-		z-index: -2;
-	}
-	div:is(:hover, :focus-within):after {
+	div:is(:hover, :focus-within)::after {
 		opacity: 0;
 	}
 </style>
