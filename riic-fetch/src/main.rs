8995fdd602440b7d6dbc35f67e7cd52ed97c89df
astral4 <88992929+astral4@users.noreply.github.com>
2023-11-01 20:18:59 -0400

    start riic-fetch rewrite

--- riic-fetch/src/main.rs
+++ riic-fetch/src/main.rs
@@ -1,18 +1,3 @@
-use riic_fetch::Config;
-use std::env::{current_dir, set_current_dir};
-
-#[tokio::main]
-async fn main() {
-    if !current_dir()
-        .expect("Failed to get current directory")
-        .ends_with("riic-fetch")
-    {
-        set_current_dir("riic-fetch").expect("Failed to set current directory to /riic-fetch");
-    }
-
-    Config::from_toml("config.toml")
-        .expect("Failed to read configuration file")
-        .fetch()
-        .await
-        .expect("Failed to fetch and save data");
+fn main() {
+    todo!()
 }
