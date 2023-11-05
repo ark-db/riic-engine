cbf3d4a5e37d3d7fda802579f9fabb5f43cfc897
astral4 <88992929+astral4@users.noreply.github.com>
2023-11-05 01:05:45 -0400

    fetch + save skill icons

--- riic-fetch/src/base.rs
+++ riic-fetch/src/base.rs
@@ -1,11 +1,17 @@
 use crate::Server;
 use anyhow::Result;
+use image::{
+    codecs::webp::{WebPEncoder, WebPQuality},
+    imageops::{resize, FilterType},
+    load_from_memory_with_format, ColorType, ImageFormat,
+};
 use indexmap::IndexMap;
+use rayon::iter::ParallelIterator;
 use serde::{
     de::{Error, Unexpected},
     Deserialize, Deserializer, Serialize,
 };
-use std::{fs::File, io::BufWriter, path::Path};
+use std::{cmp::min, fs::File, io::BufWriter, path::Path};
 use ureq::Agent;
 
 #[derive(Deserialize)]
@@ -77,6 +83,19 @@ impl SkillTable {
         self.0.extend(other.0);
     }
 
+    pub fn get_skill_icons<P: AsRef<Path> + Sync>(
+        &self,
+        client: &Agent,
+        target_dir: P,
+        min_size: u32,
+        quality: u8,
+    ) -> Result<()> {
+        self.0
+            .par_keys()
+            .map(|id| get_skill_icon(client, id, target_dir.as_ref(), min_size, quality))
+            .collect::<Result<()>>()
+    }
+
     pub fn save<P: AsRef<Path>>(&self, path: P) -> Result<()> {
         let file = BufWriter::new(File::create(path)?);
 
@@ -84,6 +103,59 @@ impl SkillTable {
 
         Ok(())
     }
+}
+
+fn get_skill_icon(
+    client: &Agent,
+    id: &str,
+    target_dir: &Path,
+    min_size: u32,
+    quality: u8,
+) -> Result<()> {
+    let target_path = target_dir.join(id).with_extension("webp");
+
+    if target_path.is_file() {
+        return Ok(());
+    }
+
+    let url = format!(
+        "https://raw.githubusercontent.com/astral4/arkdata/main/assets/torappu/dynamicassets/arts/building/skills/{id}.png"
+    );
+
+    let res = client.get(&url).call()?;
+
+    let mut bytes = match res.header("Content-Length") {
+        Some(len) => Vec::with_capacity(len.parse()?),
+        None => Vec::new(),
+    };
+
+    res.into_reader().read_to_end(&mut bytes)?;
+
+    let mut image = load_from_memory_with_format(&bytes, ImageFormat::Png)?.to_rgb8();
+
+    let (width, height) = image.dimensions();
+    let min_dim = min(width, height);
+
+    if min_dim < min_size {
+        let scale_factor = f64::from(min_size) / f64::from(min_dim);
+        image = resize(
+            &image,
+            (scale_factor * f64::from(width)) as u32,
+            (scale_factor * f64::from(height)) as u32,
+            FilterType::Lanczos3,
+        );
+    }
+
+    let file = File::create(target_path)?;
+
+    WebPEncoder::new_with_quality(file, WebPQuality::lossy(quality)).encode(
+        &image,
+        image.width(),
+        image.height(),
+        ColorType::Rgba8,
+    )?;
+
+    Ok(())
 }
 
 #[derive(Deserialize, Serialize)]
