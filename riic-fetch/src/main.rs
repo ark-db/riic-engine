fd94e3bc66e165ad029a104ebcac035dfe79a403
astral4 <88992929+astral4@users.noreply.github.com>
2023-11-18 13:32:34 -0500

    read config from TOML file

--- riic-fetch/src/main.rs
+++ riic-fetch/src/main.rs
@@ -18,11 +18,55 @@ use reqwest::Client;
 use riic_fetch::{
     BaseData, Fetch, GetIcons, OperatorTableDe, OperatorTableSer, Server, SkillTable, TermData,
 };
-use std::{path::Path, sync::Arc, time::Duration};
+use serde::{
+    de::{Error as DeError, Unexpected},
+    Deserialize, Deserializer,
+};
+use std::{fs::read_to_string, path::Path, sync::Arc, time::Duration};
 use tokio::{
     task::{spawn, spawn_blocking, JoinHandle},
     try_join,
 };
+
+const CONFIG_PATH: &'static str = "config.toml";
+
+#[derive(Deserialize)]
+struct Config {
+    operator: SaveConfig,
+    skill: SaveConfig,
+    terms_path: Box<Path>,
+    styles_path: Box<Path>,
+    min_image_size: u32,
+}
+
+impl Config {
+    fn from_toml<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let data = read_to_string(path)?;
+        let config = toml::from_str(&data)?;
+        Ok(config)
+    }
+}
+
+#[derive(Deserialize)]
+pub(crate) struct SaveConfig {
+    pub(crate) data_path: Box<Path>,
+    pub(crate) image_dir: Box<Path>,
+    #[serde(deserialize_with = "deserialize_quality")]
+    pub(crate) quality: u8,
+}
+
+fn deserialize_quality<'de, D>(deserializer: D) -> Result<u8, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    match Deserialize::deserialize(deserializer)? {
+        n @ 1..=100 => Ok(n),
+        other => Err(DeError::invalid_value(
+            Unexpected::Unsigned(u64::from(other)),
+            &"Expected a u8 from 1 to 100",
+        )),
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -35,11 +79,12 @@ async fn main() -> Result<()> {
 
     let ops_handle = spawn(OperatorTableDe::fetch(client.clone(), Server::CN));
 
-    let c0 = client.clone();
+    let config = Config::from_toml(CONFIG_PATH)?;
 
+    let c0 = client.clone();
     let skills_handle = spawn(async move {
         let c1 = client.clone();
-        let cn_handle: JoinHandle<Result<_>> = spawn(async {
+        let cn_handle: JoinHandle<Result<_>> = spawn(async move {
             // fetch CN base data
             let BaseData { ops, skills } = BaseData::fetch(c1.clone(), Server::CN).await?;
             let (ops, skills) = (Arc::new(ops), Arc::new(skills));
@@ -49,13 +94,18 @@ async fn main() -> Result<()> {
                 // transform operator data
                 let ser = OperatorTableSer::create(ops_handle.await??, &o);
                 // save operator data
-                ser.save("../dump/chars.json")
+                ser.save(config.operator.data_path)
             });
 
             let c2 = c1.clone();
             let h1 = spawn(async move {
                 // fetch + save operator icons
-                let mut set = ops.get_icons(c1.clone(), Path::new("../dump/ops/"), 60, 25);
+                let mut set = ops.get_icons(
+                    c1.clone(),
+                    config.operator.image_dir,
+                    config.min_image_size,
+                    config.operator.quality,
+                );
                 while let Some(res) = set.join_next().await {
                     res??;
                 }
@@ -65,7 +115,12 @@ async fn main() -> Result<()> {
             let s = skills.clone();
             let h2 = spawn(async move {
                 // fetch + save skill icons
-                let mut set = s.get_icons(c2, Path::new("../dump/skills/"), 60, 50);
+                let mut set = s.get_icons(
+                    c2,
+                    config.skill.image_dir,
+                    config.min_image_size,
+                    config.skill.quality,
+                );
                 while let Some(res) = set.join_next().await {
                     res??;
                 }
@@ -91,7 +146,7 @@ async fn main() -> Result<()> {
         let skills = SkillTable::combine(&cn_skills, &en_skills);
 
         // save base skills
-        skills.save("../dump/skills.json")?;
+        skills.save(config.skill.data_path)?;
 
         Ok::<_, Error>((h0, h1, h2))
     });
@@ -105,7 +160,7 @@ async fn main() -> Result<()> {
             let data = TermData::fetch(c2, Server::CN).await?;
 
             // save text style data
-            let h = spawn_blocking(move || data.styles.save("../dump/text-colors.json"));
+            let h = spawn_blocking(move || data.styles.save(config.styles_path));
 
             // return skill terms
             Ok((data.terms, h))
@@ -126,7 +181,7 @@ async fn main() -> Result<()> {
         cn_terms.extend(en_terms);
 
         // save skill terms
-        cn_terms.save("../dump/terms.json")?;
+        cn_terms.save(config.terms_path)?;
 
         Ok::<_, Error>(h)
     });
