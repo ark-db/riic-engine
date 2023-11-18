bed0eab4c90235972940af5926b1a2e463c7e0f3
astral4 <88992929+astral4@users.noreply.github.com>
2023-11-18 15:40:28 -0500

    specify lints in Cargo.toml (new in Rust 1.74)

--- riic-fetch/src/lib.rs
+++ riic-fetch/src/lib.rs
@@ -1,19 +1,3 @@
-#![warn(clippy::pedantic, future_incompatible, nonstandard_style, unused)]
-#![deny(
-    let_underscore_drop,
-    macro_use_extern_crate,
-    non_ascii_idents,
-    noop_method_call,
-    pointer_structural_match,
-    rust_2018_idioms,
-    trivial_casts,
-    trivial_numeric_casts,
-    unreachable_pub,
-    unused_import_braces,
-    unused_qualifications
-)]
-#![allow(clippy::module_name_repetitions)]
-
 mod base;
 mod operator_de;
 mod operator_ser;
