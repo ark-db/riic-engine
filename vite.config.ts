8b1b856beb5bb5e10ca1385fb7352332c97240b2
astral4 <88992929+astral4@users.noreply.github.com>
2023-12-14 18:03:27 -0500

    update Vite build target

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const config: UserConfig = {
 		// The Windows webview (WebView2) automatically updates by default, so the target depends on the last 3 Edge versions (i.e. Edge 116+).
 		// The macOS webview (WebKit) only updates with new OS versions, so the target depends on the last 3 OS versions (i.e. Safari 15+).
 		// The Linux webview (WebKitGTK) is based on WebKit, so the target is the same as macOS.
-		target: process.env.TAURI_PLATFORM == 'windows' ? 'edge116' : ['es2021', 'safari15'],
+		target: process.env.TAURI_PLATFORM == 'windows' ? 'edge118' : ['es2021', 'safari15'],
 		// By default, Vite inlines assets smaller than 4 KiB as base64 (see https://vitejs.dev/config/build-options.html#build-assetsinlinelimit).
 		// These assets are blocked by the CSP in production builds, so inlining is explicitly disabled.
 		assetsInlineLimit: 0
