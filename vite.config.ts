ff03650babfd6eaab8b2f0f99ebdcbabf0e87790
astral4 <88992929+astral4@users.noreply.github.com>
2024-01-25 23:34:02 -0500

    update minimum build targets and system requirements for AVIF support

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ const config: UserConfig = {
 		stringify: true
 	},
 	build: {
-		// The Windows webview (WebView2) automatically updates by default, so the target depends on the last 3 Edge versions (i.e. Edge 116+).
-		// The macOS webview (WebKit) only updates with new OS versions, so the target depends on the last 3 OS versions (i.e. Safari 15+).
+		// The Windows webview (WebView2) automatically updates by default, so the target depends on the last 3 Edge versions.
+		// The macOS webview (WebKit) only updates with new OS versions, so the target depends on the last 3 OS versions.
 		// The Linux webview (WebKitGTK) is based on WebKit, so the target is the same as macOS.
-		target: process.env.TAURI_PLATFORM == 'windows' ? 'edge118' : ['es2021', 'safari15'],
+		target: process.env.TAURI_PLATFORM == 'windows' ? 'edge121' : ['es2022', 'safari16.4'],
 		// By default, Vite inlines assets smaller than 4 KiB as base64 (see https://vitejs.dev/config/build-options.html#build-assetsinlinelimit).
 		// These assets are blocked by the CSP in production builds, so inlining is explicitly disabled.
 		assetsInlineLimit: 0
